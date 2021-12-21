b18ce4ad731dccbf9dab31562f8f10c83a82a2ec
kenny ongko <kennyongkowinata@gmail.com>
2021-12-21 21:58:39 +0700

    miration table workshop_picture

---
+++ backend-api/database/migrations/2021_12_21_145424_create_workshop_picture_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkshopPictureTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshop_picture', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('workshopID');
+            $table->foreign('workshopID')->references('id')->on('workshops');
+            $table->string('workshopPicture')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshop_picture');
+    }
+}
