9477cfb2e3cb14a197bdf931f775d4421f13e88a
kenny ongko <kennyongkowinata@gmail.com>
2021-12-23 22:51:24 +0700

    Update WorkshopController.php

--- backend-api/app/Http/Controllers/Api/WorkshopController.php
+++ backend-api/app/Http/Controllers/Api/WorkshopController.php
@@ -71,11 +71,31 @@ class WorkshopController extends Controller
             }
         }
         try{
+            $workshops = DB::table('workshops')
+            ->get()->toArray();
+            $operational_workshops =  DB::table('operational_workshops')
+            ->get()->toArray();
+            $workshop_details =  DB::table('workshop_details')
+            ->get()->toArray();
+            $workshop_services =  DB::table('workshop_services')
+            ->get()->toArray();
+
+            foreach($workshops as &$value)
+            {
+                $value->operational_workshop = array_filter($operational_workshops, function($operational_workshops) use ($value) {
+                    return $operational_workshops->workshopID === $value->id;
+                });
+                $value->workshop_details = array_filter($workshop_details, function($workshop_details) use ($value) {
+                    return $workshop_details->workshopID === $value->id;
+                });
+                foreach ($workshop_details as &$value2) {
+                    $value2->workshop_services = array_filter($workshop_services, function($workshop_services) use ($value2) {
+                        return $workshop_services->workshopDetailID === $value2->id;
+                    });
+                }
+            }
             $data = [
-                'objectReturn' => DB::table('workshops')
-                ->get(),
-                'objectReturn1' => DB::table('operational_workshops')
-                ->get(),
+                'objectReturn'=>$workshops
             ];
             return response()->json($data , 200);
         } catch (Exception $err){
