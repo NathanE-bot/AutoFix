7cc095693bb3f2092d1f10278c530a266ce7dcef
kenny ongko <kennyongkowinata@gmail.com>
2021-12-31 20:05:33 +0700

    Update WorkshopController.php

--- backend-api/app/Http/Controllers/Api/WorkshopController.php
+++ backend-api/app/Http/Controllers/Api/WorkshopController.php
@@ -99,28 +99,28 @@ class WorkshopController extends Controller
             ->Where('district','like','%'.$request->location.'%')
             ->Where('statusHr','like','%'.$request->statusHr.'%')
             ->Where('status24Hr','like','%'.$request->status24Hr.'%')
-            ->paginate(10)->toArray();
-            $workshop_services =  DB::table('workshop_services')
-            ->get()->toArray();
-            $operational_workshops =  DB::table('operational_workshops')
-            ->get()->toArray();
-            $workshop_details =  DB::table('workshop_details')
-            ->get()->toArray();
+            ->paginate(10);
+            // $workshop_services =  DB::table('workshop_services')
+            // ->get()->toArray();
+            // $operational_workshops =  DB::table('operational_workshops')
+            // ->get()->toArray();
+            // $workshop_details =  DB::table('workshop_details')
+            // ->get()->toArray();
 
-            foreach($workshops['data'] as &$value)
-            {
-                $value->operational_workshop = array_filter($operational_workshops, function($operational_workshops) use ($value) {
-                    return $operational_workshops->workshopID === $value->id;
-                });
-                $value->workshop_details = array_filter($workshop_details, function($workshop_details) use ($value) {
-                    return $workshop_details->workshopID === $value->id;
-                });
-                foreach ($workshop_details as &$value2) {
-                    $value2->workshop_services = array_filter($workshop_services, function($workshop_services) use ($value2) {
-                        return $workshop_services->workshopDetailID === $value2->id;
-                    });
-                }
-            }
+            // foreach($workshops['data'] as &$value)
+            // {
+            //     $value->operational_workshop = array_filter($operational_workshops, function($operational_workshops) use ($value) {
+            //         return $operational_workshops->workshopID === $value->id;
+            //     });
+            //     $value->workshop_details = array_filter($workshop_details, function($workshop_details) use ($value) {
+            //         return $workshop_details->workshopID === $value->id;
+            //     });
+            //     foreach ($workshop_details as &$value2) {
+            //         $value2->workshop_services = array_filter($workshop_services, function($workshop_services) use ($value2) {
+            //             return $workshop_services->workshopDetailID === $value2->id;
+            //         });
+            //     }
+            // }
 
             $data = [
                 'objectReturn'=>$workshops
