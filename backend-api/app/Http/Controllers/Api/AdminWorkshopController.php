c62264f9228b6b6abe92f19f8c72ade9d338e543
kenny ongko <kennyongkowinata@gmail.com>
2022-01-18 01:20:41 +0700

    Update AdminWorkshopController.php

--- backend-api/app/Http/Controllers/Api/AdminWorkshopController.php
+++ backend-api/app/Http/Controllers/Api/AdminWorkshopController.php
@@ -22,7 +22,7 @@ class AdminWorkshopController extends Controller
         $IncomingSchedule = DB::table('schedules')
         ->join('workshops','workshops.id','=','schedules.workshopID')
         ->join('users','users.ID','=','workshops.userID')
-        ->select('schedules.id as ScheduleID','schedules.userID as CustomerID','workshops.userID as AdminID','schedules.workshopID'
+        ->select('schedules.id as scheduleID','schedules.userID as customerID','workshops.userID as AdminID','schedules.workshopID'
         ,'schedules.workshopName','schedules.workshopAddress','schedules.workshopPhoneNumber',
         'schedules.workshopEmail','schedules.scheduleDate','schedules.scheduleTime','schedules.carModel',
         'schedules.carType','schedules.timeEstimation','schedules.priceEstimation','schedules.scheduleStatus'
@@ -45,8 +45,6 @@ class AdminWorkshopController extends Controller
     }
 
     public function accpetScheduleByAdmin(Request $req){
-
-
         if ($req->scheduleStatus=='accepted') {
             $validator = Validator::make($req->all(), [
                 'description' => 'string','required'
@@ -86,5 +84,57 @@ class AdminWorkshopController extends Controller
         $dataUpdatedSchedule = DB::table('schedules')->where('id','=',$req->scheduleID)->get();
 
         return response()->json($dataUpdatedSchedule, 200);
+    }
+
+    public function updateWorkshopForAdminBengkel (Request $req){
+
+    }
+
+    public function getWorkshopProfileByUserID (Request $req){
+        try{
+            $workshops = DB::table('workshops')
+            ->where('userID','=',$req->adminID)
+            ->get()->toArray();
+
+            foreach ($workshops as $key=>$value) {
+            $operational_workshops =  DB::table('operational_workshops')
+            ->orWhere('operational_workshops    .workshopID','=',$value->id)
+            ->get()->toArray();
+
+            $workshop_details =  DB::table('workshop_details')
+            ->orWhere('workshop_details.workshopID','=',$value->id)
+            ->get()->toArray();
+
+            $workshop_services =  DB::table('workshop_services')
+            ->join('workshop_details','workshop_details.id','=','workshop_services.workshopDetailID')
+            ->select('workshop_services.id','workshop_details.workshopID','workshopDetailID','serviceType','serviceDetail','price','time')
+            ->orWhere('workshop_details.workshopID','=',$value->id)
+            ->get()->toArray();
+            }
+            $workshop_review = DB::table('reviews')
+            ->join('schedules','schedules.id','=','reviews.scheduleID')
+            ->select('reviews.id','reviews.scheduleID','schedules.workshopID','reviews.userName','reviews.reviewDate','reviews.rating','reviews.description')
+            ->where('schedules.scheduleStatus','=','done')
+            ->get()->toArray();
+
+
+            foreach($workshops as &$value)
+            {
+                $value->operational_workshop = array_filter($operational_workshops, function($operational_workshops) use ($value) {
+                    return $operational_workshops->workshopID === $value->id;
+                });
+                $value->workshop_details = array_filter($workshop_details, function($workshop_details) use ($value) {
+                    return $workshop_details->workshopID === $value->id;
+                });
+
+                $value->workshop_services = array_filter($workshop_services, function($workshop_services) use ($value) {
+                    return $workshop_services->workshopID === $value->id;
+                });
+            }
+
+            return response()->json($workshops[0], 200);
+        } catch (Exception $err){
+            return response()->json($err, 500);
+        }
     }
 }
