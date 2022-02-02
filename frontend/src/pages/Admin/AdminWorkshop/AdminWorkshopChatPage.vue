49c30702d263019dd8cd6af74dccfd90fbd37f59
Nathan E <nathanengolodoe@indotrading.com>
2022-02-02 22:57:32 +0700

    fix admin header

--- frontend/src/pages/Admin/AdminWorkshop/AdminWorkshopChatPage.vue
+++ frontend/src/pages/Admin/AdminWorkshop/AdminWorkshopChatPage.vue
@@ -1,18 +1,21 @@
 <template>
   <q-page>
-      <div>
-          ASD
-      </div>
+    <div>
+      ASD
+    </div>
   </q-page>
 </template>
 
 <script>
 /* eslint-disable */
+import help from '../../../js/help'
+import Auth from '../../../js/AuthValidation'
+
 export default {
   data () {
-      return {
-          loader: false
-      }
+    return {
+      loader: false
+    }
   }
 }
 </script>
