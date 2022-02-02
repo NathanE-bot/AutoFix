96984fcc8f3d1cb151f3b941caf35e8cecf8dd56
Nathan E <nathanengolodoe@indotrading.com>
2022-02-02 22:47:09 +0700

    test

---
+++ frontend/src/pages/Admin/AdminWorkshop/AdminWorkshopChatPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page>
+      <div>
+          ASD
+      </div>
+  </q-page>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  data () {
+      return {
+          loader: false
+      }
+  }
+}
+</script>
